a7d3272da2d19bfb51bb0818b229f0c3abb965e9
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-09-14 14:15:04 -0500

    first commit of all files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const month = ref('')
+
+</script>
+
+<template>
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        Welcome back, for what month are we buying?, <input type="text" placeholder="Enter Month" v-model="month">
+      </h2>
+    </section>
+
+    <section class="create-Groceries">
+
+
+    </section>
+
+    <section class="Groceries-list">
+
+
+    </section>
+
+
+  </main>
+
+</template>
