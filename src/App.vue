4243e23b825b8b31c5a8b6bc70c52346afae10aa
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-09-14 15:10:50 -0500

    finished second section of form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup>
 import { ref } from 'vue';
 
+const myArray = ref([])
 const month = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+
+const addGroceries = () =>{
+  if(input_content.value.trim() === '' || input_category.value == nul){
+    return
+  }
+  myArray.value.push({
+    content: input_content.value,
+    category: input_category.value,
+  })
+
+  input_content.value = ''
+  input_category.value = null
+}
+
 
 </script>
 
@@ -15,7 +32,25 @@ const month = ref('')
     </section>
 
     <section class="create-Groceries">
+      <h3>ENTER GROCERIES NEEDED THIS MONTH</h3>
+      <form @submit.prevent = "addGroceries">
+        <h4>What do we need?</h4>
+        <input type="text" placeholder="e.g., Milk" v-model="input_content"/>
 
+
+      <h4>Pick a Category</h4>
+      <div class="options">
+        <label>
+          <input type="radio" name="category" value="foods" v-model="input_category">
+          <span class="bubble foods"></span>
+        </label>
+        <label>
+          <input type="radio" name="category" value="snacks" v-model="input_category">
+          <span class="bubble snacks"></span>
+        </label>
+      </div>
+      <input type="submit" value="Add Groceries"/>
+      </form>
 
     </section>
 
